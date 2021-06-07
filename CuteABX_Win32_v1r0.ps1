3da003d5ffb6792a4719043b3c6791c831223f21
manueljenkin <69294817+manueljenkin@users.noreply.github.com>
2021-06-07 01:56:49 -0700

    Update CuteABX_Win32_v1r0.ps1

--- CuteABX_Win32_v1r0.ps1
+++ CuteABX_Win32_v1r0.ps1
@@ -1,37 +1,44 @@
-$inipath = "C:\Users\user\Music\"
-$abxfile = $inipath + "ABX_result.textfile"
-$songfile0 = $inipath + "1\song.audiofile"
-$songfile1 = $inipath + "2\song.audiofile"
-$finishmessage = $inipath + ".flac"
-$player0 = "C:\MusicPlayer1\MusicPlayer1.exe"
-$player1 = "C:\MusicPlayer1\MusicPlayer2.exe"
-$delaycommon = 0
-$delayfile0 = 0
-$delayfile1 = 0
-$delayrandom = 0
-$delayrandomminimum = 0.1
-$delayrandommaximum = 0.99
+$cuteenv_software = "MusicPlayerSoftware Version 1.4 outputting Audio Streaming through User123's plugin and no other DSP plugins used, Operating System changes - disabling generic error sounds, disabling audio interface energy management profiles"
+$cuteenv_hardware = "Laptop Company_Name Model Configuration (eg 8GB RAM, Model 500 GPU with 1GB VRAM), Data cable make and model, DAC make and model and modifications if any (eg: external clock source, external Digital Isolators)"
+$cuteenv_transudcer = "Amplifier Used and Modifications if any (external power supplies, transistor/capacitor swaps, etc) and Transudcer used and modifications if any"
+$cuteenv_inipath = "C:\Users\user\Music\f\"
+$cuteenv_abxfile = $cuteenv_inipath + "ABX_result.textfile"
+$cuteenv_songfile0 = $cuteenv_inipath + "1\music.audiofile"
+$cuteenv_songfile1 = $cuteenv_inipath + "2\music.audiofile"
+$cuteenv_finishmessage = $cuteenv_inipath + "finishmessage.audiofile"
+$cuteenv_player0 = "C:\MusicPlayer1\MusicPlayer1.exe"
+$cuteenv_player1 = "C:\MusicPlayer1\MusicPlayer2.exe"
+$cuteenv_delaycommon = 0
+$cuteenv_delayfile0 = 0
+$cuteenv_delayfile1 = 0
+$cuteenv_delayrandomminimum = 0.1
+$cuteenv_delayrandommaximum = 0.99
+$cuteenv_testiterations = 6
 $ABXenvironmentcaption = (Get-WmiObject -class Win32_OperatingSystem).Caption
 $ABXenvironmentversion = [System.Environment]::OSVersion.Version
-$ABXprogramversion = v1.0r0_powershell_windows
-"CuteABX $ABXprogramversion $ABXenvironmentcaption $ABXenvironment" | Out-File -Filepath $abxfile
+$ABXprogramversion = "v1.0r0_powershell"
+"CuteABX $ABXprogramversion $ABXenvironmentcaption $ABXenvironment" | Out-File -Filepath $cuteenv_abxfile
+Add-Content $cuteenv_abxfile "Software setup is $cuteenv_software"
+Add-Content $cuteenv_abxfile "Hardware setup is $cuteenv_hardware"
+Add-Content $cuteenv_abxfile "Transducer setup is $cuteenv_transducer"
 $comparemode = Read-Host "Enter 0.To Compare Music files with Default Player 1.To compare players"
-Add-Content $abxfile "Enter 0.To Compare Music files with Default Player 1.To compare players Answer: $comparemode"
+Add-Content $cuteenv_abxfile "Enter 0.To Compare Music files with Default Player 1.To compare players Answer: $comparemode"
 $delaymode = Read-Host "Enter 0.To have no delays 1.To have pre configured delays 2.To have randomized delays"
-Add-Content $abxfile "Enter 0.To have no delays 1.To have pre configured delays 2.To have randomized delays Answer: $delaymode"
+Add-Content $cuteenv_abxfile "Enter 0.To have no delays 1.To have pre configured delays 2.To have randomized delays Answer: $delaymode"
 if($delaymode -gt 0){
-Add-Content $abxfile "delaycommon $delaycommon delayfile0 $delayfile0 delayfile1 $delayfile1"
+Add-Content $cuteenv_abxfile "delaycommon $cuteenv_delaycommon delayfile0 $cuteenv_delayfile0 delayfile1 $cuteenv_delayfile1"
 if($delaymode -eq 2){
-Add-Content $abxfile "delayrandomrange $delayrandomminimum $delayrandommaximum"}
+Add-Content $cuteenv_abxfile "delayrandomrange $cuteenv_delayrandomminimum $cuteenv_delayrandommaximum"}
 }
+$delayrandom = 0
 $y = Get-Date
-Add-Content $abxfile "Date $y"
-for (($i = 0); $i -lt 10; $i++){
+Add-Content $cuteenv_abxfile "Date $y"
+for (($i = 0); $i -lt $cuteenv_testiterations; $i++){
 $a = 0, 1 | Get-Random
 $f = 0
 $b = ($a + 1) % 2
 $g = 0
-$c = Get-Random -Minimum 0 -Maximum 2 ##(same as $a but written in a different way)
+$c = Get-Random -Minimum 0 -Maximum 2
 $h = 0
 $m = 0
 Do{
@@ -46,26 +53,36 @@ if($e -eq 3){
 $p = $c
 $h = $h + 1}
 if ($p -eq 0){
-$delayfile = $delayfile0
+$delayfile = $cuteenv_delayfile0
 if ($comparemode -eq 0){
-$q = $songfile0}
+$q = $cuteenv_songfile0}
 else {
-$q = "$player0"}}
+$q = $cuteenv_player0}
+}
 if ($p -eq 1){
-$delayfile = $delayfile1
+$delayfile = $cuteenv_delayfile1
 if ($comparemode -eq 0){
-$q = $songfile1}
+$q = $cuteenv_songfile1}
 else {
-$q = "$player1"}}
+$q = $cuteenv_player1}
+}
 if ($e -lt 4){
 if ($delaymode -eq 0){
+if ($comparemode -eq 0){
 Invoke-Item $q}
 else {
+Start-Process "$q" "$cuteenv_songfile0"}
+}
+else {
 if ($delaymode -eq 2){
-$delayrandom = Get-Random -Minimum $delayrandomminimum -Maximum $delayrandommaximum}
-$delaytotal = $delaycommon + $delayfile + $delayrandom
+$delayrandom = Get-Random -Minimum $cuteenv_delayrandomminimum -Maximum $cuteenv_delayrandommaximum}
+$delaytotal = $cuteenv_delaycommon + $delayfile + $delayrandom
 Start-Sleep -seconds $delaytotal
-Start-Process "$q" "$songfile0"}
+if ($comparemode -eq 0){
+Invoke-Item $q}
+else {
+Start-Process "$q" "$cuteenv_songfile0"}
+}}
 }
 else{
 $m = 1
@@ -77,9 +94,9 @@ $preference = Read-Host "Enter  1.I-Prefer-X 2.I-DONOT-Prefer-X 3.No-Preference-
 }
 } While ($m -le 0)
 $y = Get-Date -Format HH:mm:ss
-Add-Content $abxfile "$a || $b || $c || $d || $preference|| $f || $g || $h || $y"
+Add-Content $cuteenv_abxfile "$a || $b || $c || $d || $preference|| $f || $g || $h || $y"
 "`n"
 }
-Invoke-Item $finishmessage
+Invoke-Item $cuteenv_finishmessage
 $auxiliary = Read-Host "Other than the exit button, have you used any play/pause/rewind/other controls on the player? 1. Yes 2.No"
-Add-Content $abxfile "Other than the exit button, have you used any play/pause/rewind/other controls on the player? 1.Yes 2.No Answer: $auxiliary"
+Add-Content $cuteenv_abxfile "Other than the exit button, have you used any play/pause/rewind/other controls on the player? 1.Yes 2.No Answer: $auxiliary"
